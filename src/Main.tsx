747a1a4ff33971490b5608a7981c02c8ab472b62
ArmyaAli <ali.umar.work@gmail.com>
2021-02-13 14:35:24 -0500

    bootstrapping project

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./Main.css";
+import { HomePage } from "./Pages/HomePage";
 
 function Main() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+              <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default Main;
