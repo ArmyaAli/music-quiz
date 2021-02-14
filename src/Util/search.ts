57cc23823121fc9b8eaa5bf4617effae290121b5
ArmyaAli <ali.umar.work@gmail.com>
2021-02-14 08:16:59 -0500

    api call

--- src/Util/search.ts
+++ src/Util/search.ts
@@ -4,6 +4,7 @@ import axios from 'axios'
 const API_KEY = 'AIzaSyDnc81CSV7HmMqCqviHK1EsJ01k9BzhuXA'
 
 const runSample = async (query: String) => {
+    
     try {
         const URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&type=videos&maxResults=10&q=${query}`
         const response = await axios.get(URL);
