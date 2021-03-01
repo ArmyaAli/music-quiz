0a0315c1d3b3d29a7eca920c5ad5210a2c835984
ArmyaAli <ali.umar.work@gmail.com>
2021-02-28 19:27:32 -0500

    script to populate db with top 200 spotify songs

---
+++ pyscripts/updateDb.py
@@ -0,0 +1,35 @@
+import requests
+import pyodbc
+from bs4 import BeautifulSoup
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=198.71.227.2;DATABASE=projects;UID=projects;PWD=P@ssw0rd@#')
+cursor = cnxn.cursor()
+
+response = requests.get("https://spotifycharts.com/regional/us/daily/latest")
+soup = BeautifulSoup(response.content, 'html.parser')
+
+artists = soup.select('td.chart-table-track > span')
+titles = soup.select('td.chart-table-track > strong')
+
+# DELETE OUR SONGS LIST BEFORE GRABBING NEW 
+cursor.execute("DELETE FROM [projects].[projects].[Songs]")
+
+for i in range(len(artists)):
+    artist =  artists[i].getText()[2:]
+    title = titles[i].getText()
+    
+    if "'" in title:
+        title = title.replace("'", "''")
+    if "'" in artist:
+        artist = artist.replace("'", "''")
+
+    print(artist, title)
+    cursor.execute(f"INSERT INTO [projects].[projects].[Songs](Song_Title, Artist) VALUES ('{title}', '{artist}');")
+    cnxn.commit()
+
+cursor.execute("Select * from [projects].[projects].[Songs]")
+
+
+
+
+
