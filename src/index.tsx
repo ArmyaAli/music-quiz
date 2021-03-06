bc0fa5fb5ba591c4513efd45c4b89d26fde150fa
ArmyaAli <ali.umar.work@gmail.com>
2021-03-05 20:54:45 -0500

    updated css imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main';
+import "tailwindcss/tailwind.css"
 
 ReactDOM.render(
   <React.StrictMode>
