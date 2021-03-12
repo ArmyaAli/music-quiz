4a43a3a92d826cc54d2f44d74e9c8bd2accba493
ArmyaAli <ali.umar.work@gmail.com>
2021-03-12 17:48:18 -0500

    styled play agme button

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   return (
     <div className="flex h-screen w-full">
-      <div className="m-auto">
+      <div className="m-auto ">
         <Link to="/questionsPage">
           <button className="w-64 h-32 bg-green-400 rounded hover:bg-green-500 text-white text-3xl">
             Play Game
