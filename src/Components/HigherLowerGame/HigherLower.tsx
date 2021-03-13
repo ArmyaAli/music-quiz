a6d3a330d216dcd3b2544782a29d6b1dfdcc4145
ArmyaAli <ali.umar.work@gmail.com>
2021-03-12 20:12:33 -0500

    reimplemented logic

--- src/Components/HigherLowerGame/HigherLower.tsx
+++ src/Components/HigherLowerGame/HigherLower.tsx
@@ -4,6 +4,7 @@ import "./higherlower.css";
 import axios from "axios";
 
 import MOCK_DATA from "../../data.json";
+import { Divider } from "@material-ui/core";
 
 const embed = "https://www.youtube.com/embed/";
 let count = 0;
@@ -12,59 +13,70 @@ const numberWithCommas = (x: number) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
+const rounding = (x: number): number => {
+  const digits = Math.floor(Math.log10(x) + 1);
+  const firstTwo = Math.round(Number(String(digits).substring(0, 2)) / 10) * 10;
+  const rounded = (firstTwo / 10) * 10 ** (digits - 1);
+  return rounded;
+};
+
 export const HigherLower = (props: gameModeProps) => {
-  const [data, setData] = useState<dataType[] | null>(null);
-  const [currVideo, setVideo] = useState<dataType | null>(null);
-  const [digits, setDigits] = useState<number>(0);
-  const [firstTwo, setfirstTwo] = useState<number>(0);
-  const [presentedViews, setPresentedViews] = useState<number>(0);
-  const [ID, setID] = useState<string>("");
+  const [data, setData] = useState<dataType[] | undefined>(undefined);
+  const [currVideo1, setVideo1] = useState<dataType | null>(null);
+  const [currVideo2, setVideo2] = useState<dataType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  let roundedViews1 = 0;
+  let roundedViews2 = 0;
+
+  if (!isLoading) {
+    console.log(data, currVideo1, currVideo2)
+    roundedViews1 = rounding(currVideo1!.views);
+    roundedViews2 = rounding(currVideo2!.views);
+  }
 
   useEffect(() => {
     const getData = async () => {
       try {
-        // const response = await axios.get(
-        //   "https://ikengo.net/Projects/highlow/getrandomsong"
-        // );
-        setData(MOCK_DATA["DATA_SET"]);
-        if (data) {
-          setVideo(data[count] as dataType);
-          if (currVideo) {
-            setDigits(Math.floor(Math.log10(currVideo.views)) + 1);
-            setfirstTwo(
-              Math.round(Number(String(currVideo.views).substring(0, 2)) / 10) *
-                10
-            );
-            setPresentedViews((firstTwo / 10) * 10 ** (digits - 1));
-          }
-          setID(currVideo!.youtubeID);
-        }
+        const response = await axios.get(
+          "https://ikengo.net/Projects/highlow/getrandomsong"
+        );
+        console.log(response.data);
+        setData(response.data["DATA_SET"]);
+        setVideo1(response.data["DATA_SET"][count]);
+        setVideo2(response.data["DATA_SET"][count + 1]);
+        setIsLoading(false);
       } catch (err) {
         console.error(`Error occured:${err}`);
       }
     };
     getData();
-  }, [currVideo, data, digits, firstTwo, ID]);
+
+    //   setData(MOCK_DATA["DATA_SET"]);
+    //   setVideo1(MOCK_DATA["DATA_SET"][count]);
+    //   setVideo2(MOCK_DATA["DATA_SET"][count + 1]);
+  }, []);
 
   const checkIfCorrect = (input: string): boolean => {
-    if (currVideo) {
-      console.log(currVideo.views, presentedViews);
-      if (input === "Higher" && currVideo.views > presentedViews) {
-        props.score();
-        return true;
-      } else if (input === "Lower" && currVideo.views < presentedViews) {
-        props.score();
-        return true;
-      } else return false;
+    console.log(roundedViews1, roundedViews2);
+    if (input === "Higher" && roundedViews2 > roundedViews1) {
+      props.score();
+      return true;
+    } else if (input === "Lower" && roundedViews1 > roundedViews2) {
+      props.score();
+      return true;
+    } else if (roundedViews1 === roundedViews2) {
+      props.score();
+      return true;
     } else return false;
   };
-
+  
   const handleHigherClick = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     console.log(checkIfCorrect("Higher"));
     if (data) {
-      setVideo(data[++count]);
+      setVideo1(data[++count]);
+      setVideo2(data[count + 1]);
     }
   };
 
@@ -73,46 +85,81 @@ export const HigherLower = (props: gameModeProps) => {
   ) => {
     console.log(checkIfCorrect("Lower"));
     if (data) {
-      setVideo(data[++count]);
+      setVideo1(data[++count]);
+      setVideo2(data[count + 1]);
     }
   };
 
+  if (isLoading) {
+    return (
+      <div className="flex h-screen bg-black">
+        <div className="m-auto text-6xl">
+            <img src="../../loading.gif" alt="loading..."/>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="h-screen">
-      <div className="flex flex-col my-40">
-        <div className="mx-auto my-8 w-full">
-          <h2 className="text-center text-2xl mb-8">
-            Is this video's view count Higher or Lower than this number
-          </h2>
-          <div className="text-center text-3xl italic font-extrabold mb-8 text-yellow-300">
-            {numberWithCommas(presentedViews)}
-            {/* {numberWithCommas(28540000)} */}
+    <div className="game-container">
+      <div className="flex flex-col h-full">
+        <div className="mx-auto flex justify-center h-full my-8 w-full">
+          <div className="w-2/5">
+            <div className="text-center text-2xl italic font-extrabold mb-8 text-yellow-300">
+              {currVideo1?.title}
+            </div>
+            <iframe
+              className="mx-auto w-3/5 md:w-3/6 my-4"
+              title="video"
+              height="250"
+              width="400"
+              src={embed + currVideo1?.youtubeID}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="text-center text-2xl italic font-extrabold mb-8 text-yellow-300">
+              {numberWithCommas(roundedViews1)}
+            </div>
           </div>
-          <iframe
-            className="mx-auto w-3/5 md:w-3/6"
-            title="video"
-            height="315"
-            width="560"
-            src={embed + ID}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <div className="w-2/5">
+            <div className="text-center text-2xl italic font-extrabold mb-8 text-yellow-300">
+              {currVideo2?.title}
+            </div>
+            <iframe
+              className="mx-auto w-3/5 md:w-3/6 my-4"
+              title="video"
+              height="250"
+              width="400"
+              src={embed + currVideo2?.youtubeID}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+
+        <div className="text-center text-2xl font-extrabold mb-8 text-black-300">
+          {currVideo2?.title} has
         </div>
 
         <div className="mx-auto flex">
           <button
-            className="p-4 border rounded border-green-500 text-black bg-green-300 hover:bg-green-400 hover:text-white text-green-100 mx-4 w-24 text-center rounded"
+            className="p-4 border border-green-500 bg-green-300 hover:bg-green-400 hover:text-white text-green-100 mx-4 w-24 text-center rounded"
             onClick={handleHigherClick}
           >
             Higher
           </button>
+          <div className="text-center text-2xl font-extrabblack mb-8 text-black-300"> OR </div>
           <button
             className="p-4 border rounded border-red-500 text-black bg-red-300 hover:bg-red-400 text-red-100 mx-4 w-24 text-center rounded"
             onClick={handleLowerClick}
           >
             Lower
           </button>
+        </div>
+        <div className="text-center text-2xl font-medium mb-8 text-black-300">
+          views than {currVideo1?.title}
         </div>
       </div>
     </div>
