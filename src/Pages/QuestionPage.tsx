4cb19d11944953b1ad8731817d4d585c77d42cd4
ArmyaAli <ali.umar.work@gmail.com>
2021-03-12 21:31:19 -0500

    routes

--- src/Pages/QuestionPage.tsx
+++ src/Pages/QuestionPage.tsx
@@ -10,8 +10,8 @@ import {
 import { HigherLower } from "../Components/HigherLowerGame/HigherLower";
 import { Thumbnails } from "../Components/ThumbnailGame/Thumbnails";
 
-const possibleRoutes : string[] = ["/higherlower", "/thumbnails"];
-const randomNum = Math.floor(Math.random()*possibleRoutes.length);
+const possibleRoutes: string[] = ["/higherlower", "/thumbnails"];
+const randomNum = Math.floor(Math.random() * possibleRoutes.length);
 
 export const QuestionsPage = () => {
   let { path, url } = useRouteMatch();
@@ -22,20 +22,24 @@ export const QuestionsPage = () => {
   };
 
   return (
-    <div className="bg-"> 
+    <div className="">
       <div className="p-4 w-full h-16 flex items-center justify-between">
-        <Link to={`${url}${possibleRoutes[randomNum]}`}>
-          <button className="p-4 border rounded border-green-500 bg-green-300 text-black hover:bg-green-400 text-white">Start Game</button>
+        <Link to={`${url}${possibleRoutes[0]}`}>
+          <button className="p-4 border rounded border-green-500 bg-green-300 text-black hover:bg-green-400 text-white">
+            Start Game
+          </button>
         </Link>
         <span className="text-3xl">Score: {score}</span>
       </div>
       <hr />
       <Switch>
         <Route exact path={path}></Route>
-        <Route path={`${path}/:gametype`}>
-          {/* <HigherLower score={updateScore} /> */}
-          <Thumbnails score={updateScore} />
+        <Route path={`${path}/higherlower`}>
+          <HigherLower score={updateScore} />
         </Route>
+        {/* <Route path={`${path}/thumbnails`}>
+          <Thumbnails score={updateScore} />
+        </Route> */}
       </Switch>
     </div>
   );
